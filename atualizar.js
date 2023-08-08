bd30921e55fa2556e5c9402abfe6591087e38105
calabocaevaldosdifhfsdf <138628043+calabocaevaldosdifhfsdf@users.noreply.github.com>
2023-08-08 20:53:27 -0300

    trabalho2

--- atualizar.js
+++ atualizar.js
@@ -1,23 +1,42 @@
 const input = require('readline-sync').question
 const banco = require('fs')
-function lista(){
+const { isModuleNamespaceObject } = require('util/types')
+function atualizar(){
     var pessoa  = (input("id: "))
-        let leitor = banco.readFileSync("banco.csv",{encoding: "utf-8"})
-    console.log(leitor)
-    let c = leitor.split("\n")
+    let leitor = banco.readFileSync("banco.csv",{encoding: "utf-8"})
+    // console.log(leitor)
+    let lista = leitor.split("\n")
     let a = 2
-    while(a<c.length){
-        let d = c[a].split(";")
-
+    while(a<lista.length){
+        let d = lista[a].split(";")
+        
         let h = d[0]
-        console.log("id:" , h)
-        a++
-    
+        // console.log("id:" , h)
+        
         if(pessoa == h){
-            console.log(":)")
+            console.log(d)
+            var nome = (input("nome = "))
+            var matricula =input("matricula = ")
+            var idade = input("idade = ")
+            var nota = input("nota = ")
+            
+            d[1] = nome
+            d[2] = matricula
+            d[3] = idade
+            d[4] = nota
+            let string = d.join(';')
+            console.log(string)
+            lista[a] = d
+            // console.log(lista)
         }
+        a++
     }
     
+    banco.writeFileSync("banco.csv", lista.toString(), {flag: "w"})
+    console.log(banco)
     
 }
-lista()
+atualizar()
+
+
+
